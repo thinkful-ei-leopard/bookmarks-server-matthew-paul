333528026bd2e705eef9e570aecb5f36fd86370b
wavinginspace <paulbaisley@gmail.com>
2020-02-21 11:13:46 -0500

    added logger and auth validate function

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
+const logger = require('./logger')
 const { NODE_ENV } = require('./config');
 
 const app = express();
@@ -12,6 +13,18 @@ const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  // move to the next middleware
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
